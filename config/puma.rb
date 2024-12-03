e5ea7387231dfd534a30daf86df3486770c9b43f
Chantal <chantalgomez@pursuit.org>
2024-12-02 20:19:38 -0500

    Make render-build.sh executable

--- config/puma.rb
+++ config/puma.rb
@@ -31,11 +31,7 @@ bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
-
 puts "Puma is starting on port #{ENV.fetch("PORT", 3000)}!!!!!! "
-
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
@@ -46,3 +42,4 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
