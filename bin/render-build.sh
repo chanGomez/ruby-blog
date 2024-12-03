501bad6a9d916494771d262693f097577718e91e
Chantal <chantalgomez@pursuit.org>
2024-12-03 10:39:57 -0500

    solving merging conflicts

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,13 +3,8 @@
 set -o errexit
 
 bundle install
-<<<<<<< HEAD
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-=======
-chmod +x bin/*
-bundle exec rake db:migrate
->>>>>>> c479e2f4834e7e7070fe012f2a37d4de6406f71c
