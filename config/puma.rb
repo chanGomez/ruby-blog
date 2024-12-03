a5e9ef0cac6fb1fb1cd6beeedc3b898355dc48f2
Chantal <chantalgomez@pursuit.org>
2024-12-02 19:09:47 -0500

    changing procfile to run server command to web: bundle exec puma -C config/puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -24,12 +24,17 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
+
+# Specifies the `bind` address and port dynamically from environment variables
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
-puts "Puma is starting on port #{ENV.fetch("PORT", 3000)}"
+
+puts "Puma is starting on port #{ENV.fetch("PORT", 3000)}!!!!!! "
 
 
 # Allow puma to be restarted by `bin/rails restart` command.
